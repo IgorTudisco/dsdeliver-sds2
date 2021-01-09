27efc7294372e258a7b4abb05038b9e5b0927ea2
unknown <igortudisco@hotmail.com>
2021-01-09 16:19:48 -0300

    GET /orders

---
+++ back end/src/main/java/com/tudis/dsdeliver/controllers/OrderController.java
@@ -0,0 +1,31 @@
+package com.tudis.dsdeliver.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tudis.dsdeliver.dto.OrderDTO;
+import com.tudis.dsdeliver.services.OrderService;
+
+@RestController
+@RequestMapping(value = "/orders")
+public class OrderController {
+	
+	@Autowired
+	private OrderService service;
+	
+	@GetMapping
+	public ResponseEntity<List<OrderDTO>> findAll(){
+		List<OrderDTO> list = service.findall();
+		return ResponseEntity.ok().body(list);
+	}
+		
+
+	
+	
+
+}
---
+++ back end/src/main/java/com/tudis/dsdeliver/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.tudis.dsdeliver.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.tudis.dsdeliver.dto.OrderDTO;
+import com.tudis.dsdeliver.entities.Order;
+import com.tudis.dsdeliver.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findall() {
+		List<Order> list = repository.findOrdersWithProducts();
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+
+}
